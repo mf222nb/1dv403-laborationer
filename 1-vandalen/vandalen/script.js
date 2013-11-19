86566f07d9bb74dfd8a8927f6823ff00ab2b76fc
OpenShift guest <mf222nb@student.lnu.se>
2013-11-19 09:32:38 -0500

    uppdatering med lite kod och argument

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -4,6 +4,33 @@ var makePerson = function(persArr){
 
 
 	// Din kod här...
-
-}
+    var name;
+    var averageAge;
+    var personAges;
+    var totalSum;
+    var personObj = {};
+    
+    //Skapar en array som tar ut alla age från persArr arrayen
+    personAges = persArr.map(function(person){
+        return person.age;
+    });
+    
+    personObj.minAge = personAges.reduce(function(prevAge, age, i, personAges){
+        return Math.min(prevAge, age);
+    });
+    
+    personObj.maxAge = personAges.reduce(function(prevAge, age, i, personAges){
+        return Math.max(prevAge, age);
+    });
+    
+    totalSum = personAges.reduce(function(prevAge, age, i, personAges) {
+        return prevAge + age;
+    });
+    
+    personObj.averageAge = Math.round(totalSum/personAges.length);
+    //console.log(personAges); 
+    
+    return personObj;
+    
+};
 
