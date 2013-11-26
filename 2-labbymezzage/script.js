ab8fbf640cc6e52f530fb9f391da9e519beafd7d
OpenShift guest <mf222nb@student.lnu.se>
2013-11-26 10:04:57 -0500

    kommit en bit i labbymezzage

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -1,9 +1,17 @@
 window.onload = function () {
     
-    var mess = new Message("Testmeddelande", new Date());
+    var MessageBoard = {
+        
+        messages: []
+    };
+    
+    MessageBoard.messages.push("meddelande 0", "meddelande 1", "meddelande 3");
+    console.log(MessageBoard.messages[0]);
+    
+    /*var mess = new Message("Testmeddelande", new Date());
     alert(mess);
     alert(mess.getText());
     mess.setText("En väldigt fin dag");
-    alert(mess);
+    alert(mess);*/
 };
     
