6e24bf852fb63b55e155bbc29ae922731568112b
OpenShift guest <mf222nb@student.lnu.se>
2013-11-27 12:12:47 -0500

    kommit längre i labbymezzage

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -1,17 +1,45 @@
-window.onload = function () {
-    
-    var MessageBoard = {
+var MessageBoard = {
         
-        messages: []
-    };
-    
-    MessageBoard.messages.push("meddelande 0", "meddelande 1", "meddelande 3");
-    console.log(MessageBoard.messages[0]);
-    
-    /*var mess = new Message("Testmeddelande", new Date());
-    alert(mess);
-    alert(mess.getText());
-    mess.setText("En väldigt fin dag");
-    alert(mess);*/
+        messages: [],
+        
+        /*messageBox: function(messageBox){
+            var div = document.createElement("div");
+            div.id = "messagebox";
+            document.getElementById("form").insertBefore(div, document.getElementById("textArea"));
+        },*/
+        
+        init: function(){
+            var that = this;
+            
+            
+            var sendButton = document.getElementById("send");
+            //That = this ger mig en referens till mitt MessageBoard object. Den anonyma funktionen är en referens till den fuktionen
+            //som ska köras när eventet triggas
+            sendButton.addEventListener("click", function(e){
+                that.send(e);
+            }, false);
+        },
+        
+        send: function(e){
+            e.preventDefault();
+            var strMessage = document.getElementById("textArea").value;
+            var message = new Message(strMessage, Date());
+            this.messages.push(message);
+            console.log(message.getText());
+            console.log(this.messages.toString());
+            this.renderMessage(message);
+        },
+        
+        renderMessage: function(message){
+            var div = document.getElementById("messagebox");
+            var box = document.createElement("div");
+            var text = document.createTextNode(message);
+            box.appendChild(text);
+            div.appendChild(box);
+        }
 };
-    
+
+window.onload = function () {
+    MessageBoard.init();
+    //MessageBoard.messageBox();
+};
