11ef824928af815477effaf8ce8b31fcb89e52af
OpenShift guest <mf222nb@student.lnu.se>
2013-11-28 10:16:06 -0500

    moment 7 nästan klar på labby mezzage

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -24,29 +24,66 @@ var MessageBoard = {
             e.preventDefault();
             var strMessage = document.getElementById("textArea").value;
             var message = new Message(strMessage, new Date());
+            //Tömmer chatten så att man själv slipper suddar
             document.getElementById("textArea").value = "";
             this.messages.push(message);
             
-            this.renderMessage(message);
+            this.renderMessage(message,  (this.messagesCount() - 1));
             
-            var counter = this.messages.length;
-            var counterBox = document.getElementById("counter");
-            counterBox.innerHTML = "Antal Meddelande" + counter;
+            this.messagesCountUppdater();
         },
         
-        renderMessage: function(message){
+        messagesCountUppdater: function(){
+            var counterBox = document.getElementById("counter");
+            counterBox.innerHTML = "Antal Meddelande: " + this.messagesCount();
+        },
+        
+        messagesCount: function(){
+            return this.messages.length;  
+        },
+        
+        deleteMessage: function(del, boxId){
+            var id = boxId.replace("chatMessage", "");
+            this.messages.splice(id,1);
+            this.messagesCountUppdater();
+            
+            var box = document.getElementById("messagebox");
+            box.innerHTML = "";
+            
+            this.renderAllMessages();
+        },
+        
+        renderAllMessages: function(){
+          for (var i = 0; i < this.messagesCount(); i++) {
+              this.renderMessage(this.messages[i], i);
+          }  
+        },
+        
+        renderMessage: function(message, id){
+            var that = this;
             var div = document.getElementById("messagebox");
             var box = document.createElement("div");
+            var pTag = document.createElement("p");
+            var pTime = document.createElement("p");
+            var deleteButton = document.createElement("a");
             
-            box.id = "chatMessage" + this.messages.length;
+            box.id = "chatMessage" + id;
             box.className = "boxMassage";
             
-            var ptag = document.createElement("p");
-            var textMsg = document.createTextNode(message);
+            var time = document.createTextNode(message.getDateText());
+            var textMsg = document.createTextNode(message.getHTMLText());
+            deleteButton.innerHTML = "Ta bort";
             
-            ptag.appendChild(textMsg);
-            box.appendChild(ptag);
+            pTag.appendChild(textMsg);
+            pTime.appendChild(time);
+            box.appendChild(pTag);
+            box.appendChild(pTime);
+            box.appendChild(deleteButton);
             div.appendChild(box);
+            
+            deleteButton.addEventListener("click", function(del){
+                that.deleteMessage(del, box.id);
+            }, false);
         }
 };
 
