6bba7d4c3d8f4d5a44ae4bad330ec9b9fade3e28
OpenShift guest <mf222nb@student.lnu.se>
2013-12-09 09:42:25 -0500

    kommit en bit på memoryt

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -3,17 +3,38 @@ var MemoryApp = {
     
     init: function(rows, cols){
         var randomResult = RandomGenerator.getPictureArray(rows, cols);
-
+        
+        MemoryApp.memoryBoard(rows, cols);
+        
         MemoryApp.property(randomResult);
     },
     
     property: function(randomResult){
         var olle = randomResult;
-        alert(olle);
+        console.log(olle);
     },
     
-    memoryBoard: function(){
+    memoryBoard: function(rows, cols){
+        var table = document.createElement("table");
+        var main = document.getElementById("main");
         
+        table.id = ("table");
+        table.className = ("table");
+        
+        for (var i = 0; i <= rows; i++) {
+            var tr = document.createElement("tr");
+
+            for (var n = 0; n < cols; n++) {
+                var img = document.createElement("img");
+                img.src = "memory/pics/0.png";
+                
+                var td = document.createElement("td");
+                td.appendChild(img);
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+        main.appendChild(table);
     }
 };
 
