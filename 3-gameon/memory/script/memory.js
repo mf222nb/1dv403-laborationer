1deff2f021a99534eda91ab474053ae49ad4fa8e
OpenShift guest <mf222nb@student.lnu.se>
2013-12-09 11:14:47 -0500

    fastnat på memory

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -21,15 +21,20 @@ var MemoryApp = {
         table.id = ("table");
         table.className = ("table");
         
-        for (var i = 0; i <= rows; i++) {
+        for (var i = 0; i < rows; i++) {
             var tr = document.createElement("tr");
 
             for (var n = 0; n < cols; n++) {
                 var img = document.createElement("img");
                 img.src = "memory/pics/0.png";
                 
+                var a = document.createElement("a");
+                a.id="backside"+n;
+                //a.href="kalle";
+                
                 var td = document.createElement("td");
-                td.appendChild(img);
+                a.appendChild(img);
+                td.appendChild(a);
                 tr.appendChild(td);
             }
             table.appendChild(tr);
