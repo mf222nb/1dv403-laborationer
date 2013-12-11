ebe7d0d0b7145710577caf97f868f809237b3306
OpenShift guest <mf222nb@student.lnu.se>
2013-12-11 10:06:25 -0500

    klar med memoryt

--- 3-gameon/memory/script/card.js
+++ 3-gameon/memory/script/card.js
@@ -4,15 +4,15 @@ function Card(cardId, memory){
     var td = document.createElement("td");
     var a = document.createElement("a");
     var img = document.createElement("img");
+    a.href="#";
     a.appendChild(img);
     td.appendChild(a);
     img.src = "memory/pics/0.png";
     var that = this;
     
-    a.addEventListener("click", function(){
-        img.src="memory/pics/" + cardId + ".png";
+    a.onclick = function(){
         memory.flipCard(that);
-    }, false);
+    };
     
     this.getTd = function(){
         return td;
@@ -22,11 +22,17 @@ function Card(cardId, memory){
         return cardId;
     };
     
+    //Vänder upp ett kort
     this.flip = function(){
-        return img.src="memory/pics/" + cardId + ".png";
+        a.onclick = null;
+        img.src="memory/pics/" + cardId + ".png";
     };
     
+    //Vänder tillbaka ett kort
     this.getReset = function() {
-        return img.src = "memory/pics/0.png";
+        a.onclick = function(){
+            memory.flipCard(that);
+        };
+        img.src = "memory/pics/0.png";
     };
 }
--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,6 +5,7 @@ var MemoryApp = {
     click: 0,
     prevCard: null,
     currentCard: null,
+    tries: 0,
     
     init: function(rows, cols){
         var randomResult = RandomGenerator.getPictureArray(rows, cols);
@@ -12,6 +13,7 @@ var MemoryApp = {
         this.memoryBoard(rows, cols, randomResult);
     },
     
+    //Skapar en tabell och ett nytt kort och lägger in kortet i en array och lägger in de olika table elementen i varandra
     memoryBoard: function(rows, cols, pictureArray){
         var table = document.createElement("table");
         var main = document.getElementById("main");
@@ -37,25 +39,53 @@ var MemoryApp = {
     },
     
     flipCard: function(card){
+        //Gör att man inte kan klicka på mer än två kort samtidigt
+        if (this.prevCard !== null && this.currentCard !== null) {
+            return;
+        }
+        
         var that = this;
         this.click++;
+        
+        //När man klickar en gång på ett kort så vänds det upp
         if (this.click === 1) {
+            card.flip();
             this.prevCard = card;
+            
+            return;
         }
+        //När man klickar på ett andra kort så vänds det upp
         if (this.click === 2) {
+            card.flip();
             this.currentCard = card;
+            
+            //Gämför om det är två likadana kort
             if (this.prevCard.getId() === this.currentCard.getId()) {
-                console.log("succes");
                 this.click = 0;
+                this.prevCard = null;
+                this.currentCard = null;
             }
             else{
+                //Låter korten vara uppe en viss tid
                 setTimeout(function() {
                     that.prevCard.getReset();
-                    that.currentCard.getReset();       
+                    that.currentCard.getReset();
+                    that.prevCard = null;
+                    that.currentCard = null;
                 }, 1000);
-                this.click = 0;
             }
+            this.click = 0;
+            this.score(this.tries++);
         }
+    },
+    //Skriver ut en liten text med antal försök det tar att klara spelet
+    score: function(tries){
+        var scoreBox = document.createElement("div");
+        var main = document.getElementById("main");
+        var p = document.getElementById("pTag").innerHTML = "Antal försök: " + this.tries;
+        
+        scoreBox.appendChild(p);
+        main.appendChild(scoreBox);
     }
 };
 
