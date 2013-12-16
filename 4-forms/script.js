7a490f1bcd9efdfb7659472ca47550ba73b16afc
OpenShift guest <mf222nb@student.lnu.se>
2013-12-16 10:00:17 -0500

    kommit lite lägre

--- 4-forms/script.js
+++ 4-forms/script.js
@@ -1,19 +1,32 @@
 "use strict";
 
 var Validator = {
-
-    onblur: function(){
-        console.log("hej");
-    },
     
-    fName: function(){
+    /*fName: function(){
         var check = document.getElementsByClassName("empty");
         for (var i = 0; i < check.length; i++) {
             check[i].onblur = Validator.onblur();    
             }
+        }*/
+        
+    validation: function(){
+        var name = document.forms["form_name"]["name"].value;
+        var surname = document.forms["form_name"]["surname"].value;
+        var email = document.forms["form_name"]["email"].value;
+        
+        if (name === "" || name === null) {
+            alert("Fyll i ett namn");
+            return false;
         }
+        
+        if (surname === "" || surname === null) {
+            alert("Fyll i ett efternamn");
+            return false;
+        }
+    }
+        
 };
 
-window.onload = function(){
+/*window.onload = function(){
     Validator.fName();
-};
+};*/
