b8e00b84e7ef8d19dbdd5b2580f89196c97d2284
OpenShift guest <mf222nb@student.lnu.se>
2013-12-16 16:43:08 -0500

    kommit lite längre på valideringen

--- 4-forms/script.js
+++ 4-forms/script.js
@@ -7,14 +7,24 @@ var Validator = {
         var fName = document.getElementById("fName");
         fName.onblur = function(){
             if (fName.value === "" || fName.value === null) {
-                alert("hej");
+                var fDiv = document.getElementById("fDiv");
+                var text = document.createTextNode("Detta fält får inte lämnas tomt");
+                var div = document.createElement("div");
+                
+                div.appendChild(text);
+                fDiv.appendChild(div);
             }
         };
         
         var sName = document.getElementById("sName");
         sName.onblur = function(){
             if (sName.value === "" || sName.value === null) {
-                alert("då");
+                var sDiv = document.getElementById("sDiv");
+                var text = document.createTextNode("Detta fält får inte lämnas tomt");
+                var div = document.createElement("div");
+                
+                div.appendChild(text);
+                sDiv.appendChild(div);
             }
         };
         
@@ -22,14 +32,24 @@ var Validator = {
         email.onblur = function(){
             //Tagit regexen från zurb foundation
             if (!email.value.match(/^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/) || email.value === "") {
-                alert("najs");
+                var eDiv = document.getElementById("eDiv");
+                var text = document.createTextNode("Du måste ange en giltig email addres");
+                var div = document.createElement("div");
+                
+                div.appendChild(text);
+                eDiv.appendChild(div);
             }    
         };
         
         var postCode = document.getElementById("postCode");
         postCode.onblur = function(){
             if (postCode.value === "") {
-                alert("tja");
+                var pDiv = document.getElementById("pDiv");
+                var text = document.createTextNode("Detta fält får inte lämnas tomt");
+                var div = document.createElement("div");
+                
+                div.appendChild(text);
+                pDiv.appendChild(div);
             }    
         };
     },
