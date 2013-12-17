7cdf27661659bfacf9eb070f1b2aa0cc92af84a9
OpenShift guest <mf222nb@student.lnu.se>
2013-12-17 10:38:42 -0500

    kommit väldigt långt i valideringen

--- 4-forms/script.js
+++ 4-forms/script.js
@@ -1,18 +1,35 @@
 "use strict";
 
 var Validator = {
+    
+    exist: null,
 
     validation: function(){
         
         var fName = document.getElementById("fName");
         fName.onblur = function(){
+            var small, text;
+            
             if (fName.value === "" || fName.value === null) {
                 var fDiv = document.getElementById("fDiv");
-                var text = document.createTextNode("Detta fält får inte lämnas tomt");
-                var div = document.createElement("div");
                 
-                div.appendChild(text);
-                fDiv.appendChild(div);
+                if(!this.exist){
+                    small = document.createElement("small");
+                    text = document.createTextNode("Detta fält får inte lämnas tomt");
+
+                    small.setAttribute("id", "firstName");
+                    small.setAttribute("class", "error");
+                    
+                    small.appendChild(text);
+                    fDiv.appendChild(small);
+                    
+                    this.exist = true;
+                }
+            }
+            else{
+                var divTag = document.getElementById("firstName");
+                divTag.parentNode.removeChild(divTag);
+                this.exist = null;
             }
         };
         
@@ -20,11 +37,24 @@ var Validator = {
         sName.onblur = function(){
             if (sName.value === "" || sName.value === null) {
                 var sDiv = document.getElementById("sDiv");
-                var text = document.createTextNode("Detta fält får inte lämnas tomt");
-                var div = document.createElement("div");
                 
-                div.appendChild(text);
-                sDiv.appendChild(div);
+                if(!this.exist){
+                    var text = document.createTextNode("Detta fält får inte lämnas tomt");
+                    var small = document.createElement("small");
+                    
+                    small.setAttribute("id", "lastName");
+                    small.setAttribute("class", "error");
+                    
+                    small.appendChild(text);
+                    sDiv.appendChild(small);
+                    
+                    this.exist = true;
+                }
+            }
+            else{
+                var divTag = document.getElementById("lastName");
+                divTag.parentNode.removeChild(divTag);
+                this.exist = null;
             }
         };
         
@@ -33,27 +63,64 @@ var Validator = {
             //Tagit regexen från zurb foundation
             if (!email.value.match(/^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/) || email.value === "") {
                 var eDiv = document.getElementById("eDiv");
-                var text = document.createTextNode("Du måste ange en giltig email addres");
-                var div = document.createElement("div");
                 
-                div.appendChild(text);
-                eDiv.appendChild(div);
-            }    
+                if(!this.exist){
+                    var text = document.createTextNode("Du måste ange en giltig email addres");
+                    var small = document.createElement("small");
+                    
+                    small.setAttribute("id", "mail");
+                    small.setAttribute("class", "error");
+                    
+                    small.appendChild(text);
+                    eDiv.appendChild(small);
+                    
+                    this.exist = true;
+                }
+            }
+            else{
+                var divTag = document.getElementById("mail");
+                divTag.parentNode.removeChild(divTag);
+                this.exist = null;
+            }
         };
         
         var postCode = document.getElementById("postCode");
         postCode.onblur = function(){
-            if (postCode.value === "") {
-                var pDiv = document.getElementById("pDiv");
-                var text = document.createTextNode("Detta fält får inte lämnas tomt");
-                var div = document.createElement("div");
+            var post = postCode.value;
+            if (post.match(/^\d{5}$/) || post.match(/^\d{3}[- ]\d{2}$/) || post.match(/^[SE]+\d{5}$/) || post.match(/^[SE]+\d{3}[- ]\d{2}$/) || post.match(/^[SE ]+(\d{3}[- ]\d{2}|\d{5})$/)) {
+                post = post.replace(/-/g, "");
+                post = post.replace(/ /g, "");
+                postCode.value = post.replace(/SE/g, "");
                 
-                div.appendChild(text);
-                pDiv.appendChild(div);
-            }    
+                var divTag = document.getElementById("post");
+                divTag.parentNode.removeChild(divTag);
+                this.exist = null;
+            }
+            else{
+                var pDiv = document.getElementById("pDiv");
+                
+                if(!this.exist){
+                    var text = document.createTextNode("Detta fält får inte lämnas tomt");
+                    var small = document.createElement("small");
+                    
+                    small.setAttribute("id", "post");
+                    small.setAttribute("class", "error");
+                    
+                    small.appendChild(text);
+                    pDiv.appendChild(small);
+                    
+                    this.exist = true;
+                }
+            }
         };
     },
     
+    submit: function(){
+        var form = document.getElementById("form_name");
+        form.onsubmit = function(){
+            return Validator.validation();
+        };
+    },
 };
 
 window.onload = function(){
